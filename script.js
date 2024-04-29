dd5d88b1a5608e1359da0f49d232cf8b930f3b0b
Joel Castagnani <joelcastagnani@hotmail.com>
2024-04-29 14:20:53 -0300

    Creacion de archivos de proyecto html js + instancia de cartas de prueba

---
+++ script.js
@@ -0,0 +1,12 @@
+//Cards instance
+const cards =[
+    {id: 1, name: "Mega Knight", elixir: 7},
+    {id: 2, name: "Lumberjack", elixir: 4},
+    {id: 3, name: "Baloon", elixir: 5},
+    {id: 4, name: "Firecracker", elixir: 3},
+    {id: 6, name: "Phoenix", elixir: 4},
+    {id: 7, name: "Golem", elixir: 8},
+    {id: 8, name: "Prince", elixir: 6},
+    {id: 9, name: "Baby Dragon", elixir: 4},
+    {id: 10, name: "Musketeer", elixir: 4}
+    ];
