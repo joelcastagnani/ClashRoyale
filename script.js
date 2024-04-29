0fbaf07828d4359c633f1acda19eb27f5a457da0
Joel Castagnani <joelcastagnani@hotmail.com>
2024-04-29 19:12:56 -0300

    Se agrega showCard, addCard, deleteCard y se arreglan las funciones para que retornen texto

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const cards =[
     {id: 2, name: "Lumberjack", elixir: 4},
     {id: 3, name: "Baloon", elixir: 5},
     {id: 4, name: "Firecracker", elixir: 3},
+    {id: 5, name: "Miner", elixir: 3},
     {id: 6, name: "Phoenix", elixir: 4},
     {id: 7, name: "Golem", elixir: 8},
     {id: 8, name: "Prince", elixir: 6},
@@ -11,14 +12,50 @@ const cards =[
     {id: 10, name: "Musketeer", elixir: 4}
     ];
 
-//Show cards list
-function showCards(){
-    let cardsList = "Lista de cartas disponibles: \n";
-    cards.forEach(card => {
-        //cardsList += `${card.id}: ${card.name} - ${card.elixir}\n`;
-        cardsList += card.id + " " + card.name + " " + card.elixir + "\n";
+function showCard(card){
+    return "ID: " + card.id + " | Name: " + card.name + " | Elixir: " + card.elixir;
+} //Show just 1 card
+function showCards(arrayObject){
+    let cardsList = "";
+    let i = 1;
+    arrayObject.forEach(card => {
+        cardsList += i + "- " + showCard(card);
+        i++;
     });
-    alert(cardsList);
-}
+    return cardsList;
+}//Show cards list
+function addCard(deck){
+    const idCard = parseInt(prompt("Ingrese el ID de la carta que quiere agregar"));
+    const card = cards.find(ca => ca.id === idCard);
+    let text = "";
+    if(card){
+        deck.push(card);
+        text = `${card.name} ha sido agregado correctamente a su mazo.`;
+    }else{
+        text = "Producto no encontrado";
+    }
+    return text;
+}//Add cards to your deck
+function deleteCard(deck){
+    const idCard = parseInt(prompt("Ingrese el ID de la carta que quiere eliminar"));
+    const index = cards.findIndex(ca => ca.id === idCard);
+    const card = cards.find(ca => ca.id === idCard);
+    let text = "";
+    if(index !== -1){
+        const deletedCard = deck.splice(index, 1)[0];
+        text = (`${card.name} ha sido eliminado correctamente de su mazo.`);
+    }else{
+        text = "Producto no encontrado en el mazo";
+    }
+    return text;
+}//Delete one card from your deck
 
-showCards();
+
+
+
+//Test Board
+const deck = [];
+//alert(showCard(cards[5]));
+//alert(addCard(deck));
+//alert(showCards(deck));
+//alert(deleteCard(deck));
